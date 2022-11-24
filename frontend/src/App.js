1c9202fab534cb43eff92fdc6952f50bdc7e2276
Marconio dos Santos <marconio.dev@gmail.com>
2022-11-24 14:52:25 -0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href='/'>Home</a>
+      </header>
+      <main>
+        Main
+      </main>
+    </div>
+  );
+}
+
+export default App;
