ff6a855142df693b6da6aeb4ecddff356c337b98
Marconio dos Santos <marconio.dev@gmail.com>
2022-11-25 12:52:33 -0300

    server runing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+const port = process.env.PORT || 2121
 
+dotenv.config()
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!')
+})
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}, you better catch it!`);
+});
