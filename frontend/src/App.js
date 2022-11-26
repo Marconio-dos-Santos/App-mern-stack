e18ec5c5cb474f25e377e6f5da56f127ffafb48e
Marconio dos Santos <marconio.dev@gmail.com>
2022-11-26 00:39:12 -0300

    add silgle product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import HomeScreen from "./pages/HomeScreen"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header";
+import HomeScreen from "./pages/HomeScreen"
+import ProductScreen from "./pages/ProductScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <>
-        <header>
-        <a href='/'>Home</a>
-        </header>
+        <Header />
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
           </Routes>
           
         </main>
