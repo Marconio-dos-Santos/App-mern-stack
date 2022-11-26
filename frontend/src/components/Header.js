1a3604e8d8880790f56bde97f40baf472a6049d2
Marconio dos Santos <marconio.dev@gmail.com>
2022-11-26 16:30:41 -0300

    add react ui bootstrap and add react router bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom"
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+import { LinkContainer } from "react-router-bootstrap"
 
 const Header = () => {
   return (
     <header>
-      <Link to={'/'}>Home</Link>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>APP</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
     </header>
   )
 }
