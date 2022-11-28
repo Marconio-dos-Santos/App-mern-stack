01de21fdf39da36f3ac557983b5ee4357e9b6766
Marconio dos Santos <marconio.dev@gmail.com>
2022-11-28 12:39:52 -0300

    heroBanner component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/esm/Container";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HeroBanner from "./components/HeroBanner";
 import HomeScreen from "./pages/HomeScreen"
 import ProductScreen from "./pages/ProductScreen";
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <Header />
+        <HeroBanner />
         <main>
           <Container>
             <Routes>
